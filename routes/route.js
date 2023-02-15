df318b306736ef4b1cc826276ec3c2417258a082
Sk Noman <sknoman77@gmail.com>
2023-02-15 12:05:55 +0600

    hold every ms sql db settings

--- routes/route.js
+++ routes/route.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const router = express.Router()
-const loginController = require('../controller/login')
-const contoller = require('../controller/api')
-const auth = require('../middleware/auth')
+// const express = require('express')
+// const router = express.Router()
+// const loginController = require('../controller/login')
+// const contoller = require('../controller/api')
+// const auth = require('../middleware/auth')
 
-router.use(express.json())
-router.use(express.urlencoded({ extended: false }))
+// router.use(express.json())
+// router.use(express.urlencoded({ extended: false }))
 
-//LOGIN
-router.post('/login',loginController.loginUser)
-//INSERT NEW CAR
-router.post('/insert',auth,contoller.insertCar)
-//GET CAR LIST
-router.get('/get-allcars',contoller.getAllCars)
-//UPDATE CAR
-router.put('/update-car',auth,contoller.updateCars)
-//DELETE CARS
-router.delete('/delete-cars',auth,contoller.deleteCars)
-//REGISTER USER
-router.post('/register',contoller.registerUser)
-//GET USERS CARS LIST
-router.get('/users-cars-list',auth,contoller.getUsersCarList)
+// //LOGIN
+// router.post('/login',loginController.loginUser)
+// //INSERT NEW CAR
+// router.post('/insert',auth,contoller.insertCar)
+// //GET CAR LIST
+// router.get('/get-allcars',contoller.getAllCars)
+// //UPDATE CAR
+// router.put('/update-car',auth,contoller.updateCars)
+// //DELETE CARS
+// router.delete('/delete-cars',auth,contoller.deleteCars)
+// //REGISTER USER
+// router.post('/register',contoller.registerUser)
+// //GET USERS CARS LIST
+// router.get('/users-cars-list',auth,contoller.getUsersCarList)
 
 
-module.exports=router;
+// module.exports=router;
