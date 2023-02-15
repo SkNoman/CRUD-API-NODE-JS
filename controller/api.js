883cc58dc4ea1eeb5020f85c923508b7cefbe97d
Sk Noman <sknoman77@gmail.com>
2023-02-15 17:09:18 +0600

    Revert "hold every ms sql db settings"
    This reverts commit df318b306736ef4b1cc826276ec3c2417258a082.

--- controller/api.js
+++ controller/api.js
@@ -1,109 +1,109 @@
-// const db = require('../db')
+const db = require('../db')
 
-// module.exports = {
-//     //REGISTER USER
-//     async registerUser(req,res){
-//         try{
-//             console.log("phone",req.body.phone.length)
-//             if(req.body.phone.length < 11){
-//                 return res.status(408).json({message: 'Please enter valid phone number'})
-//             }
-//             else if(req.body.phone.length > 11){
-//                 return res.status(408).json({message: 'Please enter valid phone number'})
-//             }
-//             else if(req.body.username.length < 4){
-//                 return res.status(408).json({message: 'Username have to contain 4 digit minimum'})
-//             }
-//             else if(req.body.username.length > 10){
-//                 return res.status(408).json({message: 'Username maximum length is 10 character'})
-//             }
-//             else if(req.body.password.length < 6){
-//                 return res.status(408).json({message: 'Password have to contain 6 digit minimum'})
-//             }
-//             else{
-//                 await db('users')
-//                 .insert({
-//                     phone: req.body.phone,
-//                     username: req.body.username,
-//                     password: req.body.password,
-//                 })
-//                 return res.status(200).json({status: 'Success', message: 'User registerd successfully'})
-//             }
+module.exports = {
+    //REGISTER USER
+    async registerUser(req,res){
+        try{
+            console.log("phone",req.body.phone.length)
+            if(req.body.phone.length < 11){
+                return res.status(408).json({message: 'Please enter valid phone number'})
+            }
+            else if(req.body.phone.length > 11){
+                return res.status(408).json({message: 'Please enter valid phone number'})
+            }
+            else if(req.body.username.length < 4){
+                return res.status(408).json({message: 'Username have to contain 4 digit minimum'})
+            }
+            else if(req.body.username.length > 10){
+                return res.status(408).json({message: 'Username maximum length is 10 character'})
+            }
+            else if(req.body.password.length < 6){
+                return res.status(408).json({message: 'Password have to contain 6 digit minimum'})
+            }
+            else{
+                await db('users')
+                .insert({
+                    phone: req.body.phone,
+                    username: req.body.username,
+                    password: req.body.password,
+                })
+                return res.status(200).json({status: 'Success', message: 'User registerd successfully'})
+            }
    
-//         }catch(error){
-//             console.log(error)
-//             return res.status(500).json({status: 'Error',message: 'Something went wrong'})
-//         }
-//     },
-//     //INSERT NEW CAR
-//     async insertCar(req,res){
-//         try{
-//             await db('carlist')
-//             .insert({
-//                 carId: req.body.car_id,
-//                 carName: req.body.car_name,
-//                 carType: req.body.car_type
-//             })
-//             return res.status(200).json({status:'Success', message: 'New Car Inserted'})
-//         }catch(error){
-//             console.log(error)
-//             return res.status(500).send('Someting went wrong')
-//         }
-//     },
-//     //GET ALL CAR LIST
-//     async getAllCars(req,res){
-//         try{
-//             const data = await db.select().from('carlist')
-//             return res.status(200).json({carlist:data})
-//         }catch(error){
-//             console.error(error)
-//             return res.status(500).send('Something went wrong')
-//         }
-//     },
-//     //UPDATE CARS
-//     async updateCars(req,res){
-//         try{
-//             const car_id = req.body.car_id
-//             await db('carlist')
-//             .update({
-//                 carName: req.body.car_name,
-//                 carType: req.body.car_type
-//             }).where({carId: car_id})
-//             let updatedData = await db('carlist').where({carId:car_id})
-//             return res.status(200).json({updatedData: updatedData})
-//         }catch(error){
-//             console.log(error)
-//         }
-//     },
-//     //DELETE CARS
-//     async deleteCars(req,res){
-//         try{
-//             const car_id = req.body.car_id
-//             await db('carlist').del().where({carId: car_id})
-//             return res.status(500).send({status: 'Success', message: 'Car Deleted Successfully'})
-//         }catch(error){
-//             console.log(error)
-//         }
-//     },
-//     //GET USERS CARS LIST
-//     async getUsersCarList(req,res){
-//         try{
-//             // db.select('carlist.carName','users.username').from('carlist')
-//             // .join('users','users.id','carlist.id')
-//             // .then(rows =>{
-//             //     console.log(rows)
-//             // })
+        }catch(error){
+            console.log(error)
+            return res.status(500).json({status: 'Error',message: 'Something went wrong'})
+        }
+    },
+    //INSERT NEW CAR
+    async insertCar(req,res){
+        try{
+            await db('carlist')
+            .insert({
+                carId: req.body.car_id,
+                carName: req.body.car_name,
+                carType: req.body.car_type
+            })
+            return res.status(200).json({status:'Success', message: 'New Car Inserted'})
+        }catch(error){
+            console.log(error)
+            return res.status(500).send('Someting went wrong')
+        }
+    },
+    //GET ALL CAR LIST
+    async getAllCars(req,res){
+        try{
+            const data = await db.select().from('carlist')
+            return res.status(200).json({carlist:data})
+        }catch(error){
+            console.error(error)
+            return res.status(500).send('Something went wrong')
+        }
+    },
+    //UPDATE CARS
+    async updateCars(req,res){
+        try{
+            const car_id = req.body.car_id
+            await db('carlist')
+            .update({
+                carName: req.body.car_name,
+                carType: req.body.car_type
+            }).where({carId: car_id})
+            let updatedData = await db('carlist').where({carId:car_id})
+            return res.status(200).json({updatedData: updatedData})
+        }catch(error){
+            console.log(error)
+        }
+    },
+    //DELETE CARS
+    async deleteCars(req,res){
+        try{
+            const car_id = req.body.car_id
+            await db('carlist').del().where({carId: car_id})
+            return res.status(500).send({status: 'Success', message: 'Car Deleted Successfully'})
+        }catch(error){
+            console.log(error)
+        }
+    },
+    //GET USERS CARS LIST
+    async getUsersCarList(req,res){
+        try{
+            // db.select('carlist.carName','users.username').from('carlist')
+            // .join('users','users.id','carlist.id')
+            // .then(rows =>{
+            //     console.log(rows)
+            // })
 
-//             await db('carlist').select('carlist.carName','users.username')
-//             .innerJoin('users','users.id','carlist.id')
-//             .then(rows =>{
-//                 console.log(rows)
-//                 return res.status(200).json({rows})
-//             })
-//         }catch(error){
-//             console.log(error)
-//             return res.status(500).json({message: 'Something went wrong'})
-//         }
-//     }
+            await db('carlist').select('carlist.carName','users.username')
+            .innerJoin('users','users.id','carlist.id')
+            .then(rows =>{
+                console.log(rows)
+                return res.status(200).json({rows})
+            })
+        }catch(error){
+            console.log(error)
+            return res.status(500).json({message: 'Something went wrong'})
+        }
+    }
     
-// }
+}
--- controller/test.js
+++ controller/test.js
@@ -1,29 +1,29 @@
-// const db = require('knex')({
-//     client: 'mysql',
-//     connection: {
-//         host: 'localhost',
-//         user: 'root',
-//         database: 'test'
-//     }
+const db = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        database: 'test'
+    }
+})
+
+
+// db.select().from('carlist')
+//   .then(rows => {
+//     console.log(rows);
+//   })
+
+//JOIN QUERY EXAMPLE
+// db.select('carlist.carName','users.username').from('carlist')
+// .join('users','users.id','carlist.id')
+// .then(rows =>{
+//     console.log(rows)
 // })
 
 
-// // db.select().from('carlist')
-// //   .then(rows => {
-// //     console.log(rows);
-// //   })
-
-// //JOIN QUERY EXAMPLE
-// // db.select('carlist.carName','users.username').from('carlist')
-// // .join('users','users.id','carlist.id')
-// // .then(rows =>{
-// //     console.log(rows)
-// // })
-
-
-// //  db.raw(`select * from carlist`).then(result => {
-// //     console.log(result)
-// // })
+//  db.raw(`select * from carlist`).then(result => {
+//     console.log(result)
+// })
 
 
 
