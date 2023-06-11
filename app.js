ab9ec4a38d3d40a8cb08bd1a8668b41d71ae305f
Sk Noman <sknoman77@gmail.com>
2023-06-11 11:14:11 +0600

    stop db conn and update app ports

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 require('dotenv').config()
 const helmet = require('helmet')
 const route = require('./routes/route')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(morgan('combined'))
 app.use((req, res, next) => {
--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-const sql_con={
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-}
+// const sql_con={
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// }
 
-const db = require('knex')({
-    client: 'mysql2',
-    connection: sql_con
-})
+// const db = require('knex')({
+//     client: 'mysql2',
+//     connection: sql_con
+// })
 
-db.raw('SELECT 1').then(function(){
-    console.log(`${process.env.DB_NAME} -database is conected using knex`)
-    console.groupEnd()
-})
+// db.raw('SELECT 1').then(function(){
+//     console.log(`${process.env.DB_NAME} -database is conected using knex`)
+//     console.groupEnd()
+// })
 
-module.exports=db
+// module.exports=db
